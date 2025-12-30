0266b7656040949be244e37d74052988d396b64c
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2025-12-30 11:47:21 +0530

    Error Handler for reducing code dublications

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+module.exports = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    success: false,
+    status: err.status || "error",
+    message: err.message || "Internal Server Error"
+  });
+};
